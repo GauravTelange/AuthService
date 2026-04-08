ae58b547e1da7f372c6c2296f8c2bb98c9ba2100
Gaurav Telange <gauravtelange4321@gmail.com>
2026-04-08 23:25:19 +0530

    Project skeleton setup done

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const {PORT} = require('./config/serverConfig');
+
+const app = express();
+
+const prepareAndStartServer = async () => {
+
+
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port: ${PORT}`);
+    });
+}
+  
+
+prepareAndStartServer();
