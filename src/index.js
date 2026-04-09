8786351dfd8acb9bd39c8627f931fe1a2883e974
Gaurav Telange <gauravtelange4321@gmail.com>
2026-04-09 21:15:26 +0530

    added get user by id in repo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
 
 const apiRoutes = require('./routes/index');
+const UserRepository = require('./repository/user-repository');
+
 
 const app = express();
 
@@ -12,8 +14,14 @@ const prepareAndStartServer = async () => {
     app.use(bodyParser.urlencoded({extended: true}));   
     app.use('/api', apiRoutes);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async() => {
+       
         console.log(`Server is running on port: ${PORT}`);
+        // const repo = new UserRepository();
+        // const reponse = await repo.getById(1);
+        //  console.log(reponse);
+
+
     });
 }
   
