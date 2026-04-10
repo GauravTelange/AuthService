17b23998335a10e11ce8dba37278ff1c2f44c8f1
Gaurav Telange <gauravtelange4321@gmail.com>
2026-04-10 14:42:11 +0530

    Added email and password validation for incoming requests

--- src/migrations/auth-request-validators.js
+++ src/migrations/auth-request-validators.js
@@ -1,6 +1,6 @@
 const validateUserAuth = (req, res) => {
     if(!req.body.email || !req.body.password){
-        return res.status(500).json({
+        return res.status(400).json({
             success:false,
             data: {},
             message: 'Something went wrong',
